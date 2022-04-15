513f7a7e628de4596b73a954c7b3a4ea7db5b24d
Thanasis Georgiou <ageorgiou@noa.gr>
2022-04-15 14:20:40 +0300

    [firmware] Use ElectronicCats/mpu6050 library for MPU in field mill firmware

--- Readme.md
+++ Readme.md
@@ -2,8 +2,8 @@
 
 This repository contains everything related to the development of Atmospheric Electricity Sensors at ReACT. Specifically, two such sensors are worked on, designed to be suitable for weather balloon deployments:
 
-* Miniature Field-Mill
-* Space Charge Sensor
+- Miniature Field-Mill
+- Space Charge Sensor
 
 Documentation is not included in this repository. You should refer to the [project's book on ReACT knowledge base](https://react.space.noa.gr/kb/books/electricity-sensors).
 
@@ -11,12 +11,14 @@ Documentation is not included in this repository. You should refer to the [proje
 
 In this repository you can find:
 
-* `decoder`: A library, CMD tool and UI tool for decoding XDATA packages transmitted by the sensors through a GRAW radiosonde. Written in C#.
-* `firmware`: Arduino sketches for the microcontrollers of the two sensors.
-* `pcb`: Board designs in Eagle
+- `decoder`: A library, CMD tool and UI tool for decoding XDATA packages transmitted by the sensors through a GRAW radiosonde. Written in C#.
+- `firmware`: Arduino sketches for the microcontrollers of the two sensors.
+- `pcb`: Board designs in Eagle
 
 ## Blame
 
-* **Team lead:** Lilly Daskalopoulou <vdaskalop@noa.gr> @ModusElectrificus
-* **Hardware Engineering**: Vasilis Spanakis-Misirlis <vspanakis@noa.gr>
-* **Consulting**: Thanasis Georgiou <ageorgiou@noa.gr> @thgeorgiou
+- **Team lead:** Lilly Daskalopoulou <vdaskalop@noa.gr> @ModusElectrificus
+- **Hardware Engineering**: Vasilis Spanakis-Misirlis <vspanakis@noa.gr>
+- **Consulting**: Thanasis Georgiou <ageorgiou@noa.gr> @thgeorgiou
+
+Field mill firmware uses code from the [ElectronicCats/mpu6050](https://github.com/ElectronicCats/mpu6050) library, available under the MIT license.
--- firmware/field-mill/field-mill.ino
+++ firmware/field-mill/field-mill.ino
@@ -11,145 +11,52 @@
 #include <Servo.h>
 #include <Wire.h>
 
+// MPU6050 libraries
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+
+
 /** Servo motor **/
 Servo myservo;
 
-/** acceleration vector */
-float accX, accY, accZ;
-/** Rotation vector */
-float gyroX, gyroY, gyroZ;
-float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ;
-/** IMU orientation */
-float roll, pitch, yaw;
-/** Calibration values for IMU */
-float accErrorX, accErrorY, gyroErrorX, gyroErrorY, gyroErrorZ;
+/** MPU: Device, buffers, angles */
+MPU6050 mpu;
+uint16_t mpuPacketSize;
+uint16_t mpuFifoCount;
+uint8_t mpuFifoBuffer[64];
 
-float elapsedTime, currentTime, previousTime;
-int c = 0;
+Quaternion q;
+VectorFloat gravity;
+float ypr[3];
+
+/** Interrupt handler for MPU */
+volatile bool mpuInterrupt = false;
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
 
 /**
  * Initializes the MPU6050 gyro through I2C
  */
-void setupMPU6050(byte id)
+void setupMPU6050()
 {
   Wire.begin();
-  Wire.beginTransmission(id);
-  Wire.write(0x6B);
-  Wire.write(0x00);
-  Wire.endTransmission(true);
-}
+  mpu.initialize();
+  pinMode(2, INPUT);
 
-/**
- * Calculate accelerometer and gyroscope error values.
- * The IMU should be placed on a flat surface before calibration.
- *
- * The error is stored inside the `accError*` and `gyroError*` variables.
- *
- * @param id              The I2C bus ID for MPU6050.
- * @param printToSerial   If true, the error values will be printed to the serial port.
- */
-void calibrateMPU6050(byte id, bool printToSerial)
-{
-  // Read accelerometer values 200 times
-  byte c;
-  for (c = 0; c < 200; c++)
-  {
-    Wire.beginTransmission(id);
-    Wire.write(0x3B);
-    Wire.endTransmission(false);
-    Wire.requestFrom(id, 6, true);
-    accX = (Wire.read() << 8 | Wire.read()) / 16384.0;
-    accY = (Wire.read() << 8 | Wire.read()) / 16384.0;
-    accZ = (Wire.read() << 8 | Wire.read()) / 16384.0;
-    // Sum all readings
-    accErrorX = accErrorX + ((atan((accY) / sqrt(pow((accX), 2) + pow((accZ), 2))) * 180 / PI));
-    accErrorY = accErrorY + ((atan(-1 * (accX) / sqrt(pow((accY), 2) + pow((accZ), 2))) * 180 / PI));
-  }
+  int status = mpu.dmpInitialize();
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1688);
 
-  // Divide the sum by 200 to get the error value
-  accErrorX = accErrorX / 200;
-  accErrorY = accErrorY / 200;
+  mpu.CalibrateAccel(6);
+  mpu.CalibrateGyro(6);
 
-  // Read gyro values 200 times
-  for (c = 0; c < 200; c++)
-  {
-    Wire.beginTransmission(id);
-    Wire.write(0x43);
-    Wire.endTransmission(false);
-    Wire.requestFrom(id, 6, true);
-    gyroX = Wire.read() << 8 | Wire.read();
-    gyroY = Wire.read() << 8 | Wire.read();
-    gyroZ = Wire.read() << 8 | Wire.read();
-    // Sum all readings
-    gyroErrorX = gyroErrorX + (gyroX / 131.0);
-    gyroErrorY = gyroErrorY + (gyroY / 131.0);
-    gyroErrorZ = gyroErrorZ + (gyroZ / 131.0);
-    c++;
-  }
-  // Divide the sum by 200 to get the error value
-  gyroErrorX = gyroErrorX / 200;
-  gyroErrorY = gyroErrorY / 200;
-  gyroErrorZ = gyroErrorZ / 200;
+  mpu.setDMPEnabled(true);
 
-  // Print the error values on the Serial Monitor
-  if (printToSerial)
-  {
-    Serial.print("accErrorX: ");
-    Serial.println(accErrorX);
-    Serial.print("accErrorY: ");
-    Serial.println(accErrorY);
-    Serial.print("gyroErrorX: ");
-    Serial.println(gyroErrorX);
-    Serial.print("gyroErrorY: ");
-    Serial.println(gyroErrorY);
-    Serial.print("gyroErrorZ: ");
-    Serial.println(gyroErrorZ);
-  }
-}
-
-void readMPU6050(byte id, float *roll, float *pitch, float *yaw)
-{
-  Wire.beginTransmission(id);
-  Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-
-  // Accelerometer data
-  Wire.requestFrom(id, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
-  // For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
-  accX = (Wire.read() << 8 | Wire.read()) / 16384.0; // X-axis value
-  accY = (Wire.read() << 8 | Wire.read()) / 16384.0; // Y-axis value
-  accZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
-
-  // Calculating Roll and Pitch from the accelerometer data
-  accAngleX = (atan(accY / sqrt(pow(accX, 2) + pow(accZ, 2))) * 180 / PI) - 0.58;      // accErrorX ~(0.58) See the calculate_IMU_error()custom function for more details
-  accAngleY = (atan(-1 * accX / sqrt(pow(accY, 2) + pow(accZ, 2))) * 180 / PI) + 1.58; // accErrorY ~(-1.58)
-
-  // Gyroscope data
-  previousTime = currentTime;
-  currentTime = millis();
-  elapsedTime = (currentTime - previousTime) / 1000; // Divide by 1000 to get seconds
-  Wire.beginTransmission(id);
-  Wire.write(0x43); // Gyro data first register address 0x43
-  Wire.endTransmission(false);
-  Wire.requestFrom(id, 6, true);                    // Read 4 registers total, each axis value is stored in 2 registers
-  gyroX = (Wire.read() << 8 | Wire.read()) / 131.0; // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
-  gyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
-  gyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
-  // Correct the outputs with the calculated error values
-  gyroX = gyroX + 0.56; // gyroErrorX ~(-0.56)
-  gyroY = gyroY - 2;    // gyroErrorY ~(2)
-  gyroZ = gyroZ + 0.79; // gyroErrorZ ~ (-0.8)
-  // Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by sendonds (s) to get the angle in degrees
-  gyroAngleX = gyroAngleX + gyroX * elapsedTime; // deg/s * s = deg
-  gyroAngleY = gyroAngleY + gyroY * elapsedTime;
-
-  // Original code: &yaw = &yaw + gyroZ * elapsedTime;
-  // Undefined original &yaw?
-  *yaw = gyroZ * elapsedTime;
-
-  // Complementary filter - combine acceleromter and gyro angle values
-  *roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
-  *pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
+  attachInterrupt(digitalPinToInterrupt(2), dmpDataReady, RISING);
+  mpuPacketSize = mpu.dmpGetFIFOPacketSize();
 }
 
 int getAnalogPeakToPeak()
@@ -176,24 +83,17 @@ int getAnalogPeakToPeak()
 
 void setup()
 {
-  while (!Serial)
-    ;
-
   // XData UART interface: 9600baud rate, 8N1
   Serial.begin(9600);
 
-  // Gyro initialization
-  setupMPU6050(0x68);
-  calibrateMPU6050(0x68, false);
-  delay(20); // TODO Why?
-
-  // Servo initialization
-  // TODO What are we doing here
+  // Initialize LED
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(2000);
-  digitalWrite(LED_BUILTIN, LOW);
 
+  // Gyro initialization
+  setupMPU6050();
+
+  // Servo initialization
   myservo.attach(10);
   delay(2000);
   myservo.write(20); // Speed Controller Startup PWM
@@ -201,7 +101,7 @@ void setup()
   myservo.write(120); // Normal Spin
   myservo.write(120); // Normal Spin
 
-  // TODO What are we doing here
+  // Flash LED to indicate that initialization is complete
   digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
   digitalWrite(LED_BUILTIN, LOW);
@@ -212,7 +112,7 @@ void setup()
 }
 
 #ifdef SERIAL_BINARY
-void sendSerial(int counts, float roll, float pitch, float yaw)
+void sendSerial(int counts)
 {
   Serial.write('N');
   Serial.write('O');
@@ -221,15 +121,15 @@ void sendSerial(int counts, float roll, float pitch, float yaw)
   Serial.write(0xFF & (counts >> 8));
   Serial.write(0xFF & (counts));
 
-  int rollInt = roll * 100;
+  int rollInt = (ypr[2] * 180/M_PI) * 100;
   Serial.write(0xFF & (rollInt >> 8));
   Serial.write(0xFF & (rollInt));
 
-  int pitchInt = pitch * 100;
+  int pitchInt = (ypr[1] * 180/M_PI) * 100;
   Serial.write(0xFF & (pitchInt >> 8));
   Serial.write(0xFF & (pitchInt));
 
-  int yawInt = yaw * 100;
+  int yawInt = (ypr[0] * 180/M_PI) * 100;
   Serial.write(0xFF & (yawInt >> 8));
   Serial.write(0xFF & (yawInt));
 
@@ -238,7 +138,7 @@ void sendSerial(int counts, float roll, float pitch, float yaw)
 #endif
 
 #ifdef SERIAL_TEXT
-void sendSerial(int counts, float roll, float pitch, float yaw)
+void sendSerial(int counts)
 {
   Serial.print("counts: ");
   Serial.print(counts);
@@ -260,8 +160,11 @@ void loop()
   int millCounts = getAnalogPeakToPeak();
 
   // Get orientation from the MPU6050
-  readMPU6050(0x68, &roll, &pitch, &yaw);
+  mpu.dmpGetCurrentFIFOPacket(mpuFifoBuffer);
+  mpu.dmpGetQuaternion(&q, mpuFifoBuffer);
+  mpu.dmpGetGravity(&gravity, &q);
+  mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
 
   // Transmit the measurements through the UART interface to the space charge sensor
-  sendSerial(millCounts, roll, pitch, yaw);
+  sendSerial(millCounts);
 }
