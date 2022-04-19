1444138a6aa408e30c4dd7b4ba74fdbdc785e87c
Thanasis Georgiou <ageorgiou@noa.gr>
2022-04-19 13:23:35 +0300

    [decoder] Add basic open/close/plotting to UI tool

--- decoder/UI/MainWindow.xaml.cs
+++ decoder/UI/MainWindow.xaml.cs
@@ -12,7 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using NOAReact.ElectricitySensorDecoder.Library;
+using System.IO;
 
 namespace NOAReact.ElectricitySensorDecoder.UI;
 
@@ -21,21 +22,152 @@ namespace NOAReact.ElectricitySensorDecoder.UI;
 /// </summary>
 public partial class MainWindow : Window
 {
+    FileSystemWatcher? watcher = null;
+
     public MainWindow()
     {
         InitializeComponent();
     }
 
+    /// <summary>
+    /// Handler for the "Open" button click. Opens the dialog to select a file.
+    /// </summary>
     private void btnOpen_Click(object sender, RoutedEventArgs e)
     {
-        var openFileDialog = new OpenFileDialog();
-        openFileDialog.Filter = "GSF Files (*.gsf)|*gsf|All Files|*";
-        openFileDialog.Title = "Decoder: Open GSF File...";
+        var openFileDialog = new OpenFileDialog
+        {
+            Filter = "GSF Files (*.gsf)|*gsf|All Files|*",
+            Title = "Decoder: Open GSF File..."
+        };
 
         if (openFileDialog.ShowDialog() == true)
         {
-            MessageBox.Show(openFileDialog.FileName);
+            SelectFile(openFileDialog.FileName);
         }
+    }
+
+    /// <summary>
+    /// Handler for the "Close" button click. Disabled the currently active watcher.
+    /// </summary>
+    private void btnClose_Click(object sender, RoutedEventArgs e)
+    {
+        if (state.SelectedPath != null)
+        {
+            state.SelectedPath = null;
+        }
+        if (watcher != null)
+        {
+            watcher.EnableRaisingEvents = false;
+            watcher.Dispose();
+            watcher = null;
+        }
+    }
+
+    /// <summary>
+    /// Upon selection of a new GSF file, prepares the file watcher.
+    /// </summary>
+    /// <param name="newPath">The new selected GSF file</param>
+    private void SelectFile(string newPath)
+    {
+        state.SelectedPath = newPath;
+        state.DataPoints.Clear();
+        state.DataPoints["mill_counts"] = new List<double>();
+        state.DataPoints["space_counts"] = new List<double>();
+        state.LastPacketIndex = 0;
+
+        var inputParent = Directory.GetParent(newPath);
+        if (inputParent == null || !inputParent.Exists)
+        {
+            MessageBox.Show("File's parent directory is not accessible! Cannot continue.");
+            state.SelectedPath = null;
+            return;
+        }
+
+        watcher = new FileSystemWatcher(inputParent.FullName);
+        watcher.Filter = Path.GetFileNameWithoutExtension(newPath) + "*";
+        watcher.Changed += HandleChangedFile;
+        watcher.Created += HandleChangedFile;
+        watcher.EnableRaisingEvents = true;
+
+        HandleChangedFile(this, null);
+    }
+
+    /// <summary>
+    /// Called by the file watcher when the GSF files are updated. We read the data here and create the timeseries for the plot.
+    /// </summary>
+    private void HandleChangedFile(object sender, FileSystemEventArgs e)
+    {
+        if (state.SelectedPath == null)
+        {
+            return;
+        }
+
+        var parser = new GSFParser(state.SelectedPath);
+        var startTime = parser.GetStartTime();
+
+        var packets = parser.GetXData();
+
+        int i = 0;
+        foreach (var packet in packets)
+        {
+            if (i < state.LastPacketIndex)
+            {
+                i++;
+                continue;
+            }
+
+            var message = new XDataMessage(startTime, packet);
+            if (message.IsValid)
+            {
+                state.DataPoints["space_counts"].Add(Convert.ToDouble(message.Variables["space_counts"]));
+                state.DataPoints["mill_counts"].Add(Convert.ToDouble(message.Variables["mill_counts"]));
+            }
+        }
+
+        plotArea.Plot.Clear();
+        foreach (var variableName in state.DataPoints.Keys)
+        {
+            plotArea.Plot.AddSignal(state.DataPoints[variableName].ToArray());
+        }
+        plotArea.Refresh();
+    }
+
+    /// <summary>
+    /// Handler for the "to CSV" button
+    /// </summary>
+    private void btnToCSV_Click(object sender, RoutedEventArgs e)
+    {
+        if (state.SelectedPath == null)
+        {
+            return;
+        }
+
+        // Open input file
+        var parser = new GSFParser(state.SelectedPath);
+        var startTime = parser.GetStartTime();
+        var formattedTime = startTime.ToString("yyyy-MM-dd_HH-mm-ss");
+
+        var saveFileDialog = new SaveFileDialog
+        {
+            Filter = "CSV Files (*.csv)|*csv|All Files|*",
+            Title = "Decoder: Save data as CSV file",
+            FileName = formattedTime + ".csv"
+        };
+
+        if (saveFileDialog.ShowDialog() == true)
+        {
+            var csvFile = new CSVFile(saveFileDialog.FileName);
+            csvFile.WriteHeader();
+
+            var packets = parser.GetXData();
+            foreach (var packet in packets)
+            {
+                var xdata = new XDataMessage(startTime, packet);
+                csvFile.WriteXDataMessage(xdata);
+            }
+        }
+
+        MessageBox.Show("File saved!");
     }
 }
 
---
+++ decoder/UI/MainWindowState.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOAReact.ElectricitySensorDecoder.UI;
+
+public class MainWindowState : INotifyPropertyChanged
+{
+    /// <summary>
+    /// Path to currently selected file
+    /// </summary>
+    string? selectedPath = null;
+
+    /// <summary>
+    /// Whether the "Open" button should be enabled
+    /// </summary>
+    public bool OpenEnabled { get => selectedPath == null; }
+    
+    /// <summary>
+    /// Whether the "Close" button should be enabled
+    /// </summary>
+    public bool CloseEnabled { get => selectedPath != null; }
+
+    /// <summary>
+    /// Path to currently selected file
+    /// </summary>
+    public string? SelectedPath
+    {
+        get => selectedPath;
+        set
+        {
+            selectedPath = value;
+            OnPropertyChanged(nameof(SelectedPath));
+            OnPropertyChanged(nameof(OpenEnabled));
+            OnPropertyChanged(nameof(CloseEnabled));
+        }
+    }
+
+    /// <summary>
+    /// Data point values (X-axis) for the plot, each variable corresponds to one list
+    /// </summary>
+    public Dictionary<string, List<double>> DataPoints = new();
+
+    /// <summary>
+    /// Index of last packet in DataPoints array.
+    /// Used so we know only to append new points.
+    /// </summary>
+    public int LastPacketIndex = 0;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnPropertyChanged(string info)
+    {
+        Console.WriteLine(info);
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+    }
+}
